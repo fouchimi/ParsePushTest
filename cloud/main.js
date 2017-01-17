1f4617089a0da39ab430c20f189d649c92f5c66b
Ousmane <ousmanefouchimi1990@gmail.com>
2017-01-16 18:18:50 -0700

    Remove iOS code in cloud folder

--- cloud/main.js
+++ cloud/main.js
@@ -6,12 +6,13 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
   var params = request.params;
   var user = request.user;
 
-  // To be used with:
-  // https://github.com/codepath/ParsePushNotificationExample
-  // See https://github.com/codepath/ParsePushNotificationExample/blob/master/app/src/main/java/com/test/MyCustomReceiver.java
   var customData = params.customData;
   var launch = params.launch;
   var broadcast = params.broadcast;
+
+  console.log(customData);
+  console.log(launch);
+  console.log(broadcast);
 
   // use to custom tweak whatever payload you wish to send
   var pushQuery = new Parse.Query(Parse.Installation);
@@ -43,29 +44,3 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
   response.success('success');
 });
 
-// iOS push testing
-Parse.Cloud.define("iosPushTest", function(request, response) {
-
-  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
-  var params = request.params;
-  var user = request.user;
-
-  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
-  var messageText = params.text;
-
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
-
-  Parse.Push.send({
-    where: pushQuery, // Set our Installation query                                                                                                                                                              
-    data: {
-      alert: "Message: " + messageText
-    }
-  }, { success: function() {
-      console.log("#### PUSH OK");
-  }, error: function(error) {
-      console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-  response.success('success');
-});
